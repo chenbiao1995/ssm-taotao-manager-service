dfe0e4a3fb7359213097af9586483382470fe600
lenovo <lenovo@DESKTOP-IQCPEHJ>
2018-02-07 17:09:19 +0800

    商品列表展示功能

---
+++ src/main/java/com/taotao/service/impl/ItemCatServiceImpl.java
@@ -0,0 +1,48 @@
+package com.taotao.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.pojo.EasyUITreeNode;
+import com.taotao.mapper.TbItemCatMapper;
+import com.taotao.pojo.TbItemCat;
+import com.taotao.pojo.TbItemCatExample;
+import com.taotao.pojo.TbItemCatExample.Criteria;
+import com.taotao.service.ItemCatService;
+
+/**商品分类管理service
+ * @Description:
+ * @date:2018年2月7日
+ * @time:下午4:26:23
+ */
+@Service
+public class ItemCatServiceImpl implements ItemCatService {
+	
+	@Autowired
+	private TbItemCatMapper itemCatMapper;
+	@Override
+	public List<EasyUITreeNode> getItemCatList(long parentId) {
+		//根据parentId查询分类列表
+		TbItemCatExample example = new TbItemCatExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andParentIdEqualTo(parentId);
+		//执行查询
+		List<TbItemCat> list = itemCatMapper.selectByExample(example);
+		//转换成EasyUITreeNode列表
+		List<EasyUITreeNode> resultList = new ArrayList<>();
+		for (TbItemCat tbItemCat : list) {
+			//创建一个节点对象
+			EasyUITreeNode node = new EasyUITreeNode();
+			node.setId(tbItemCat.getId());
+			node.setText(tbItemCat.getName());
+			node.setState(tbItemCat.getIsParent()?"closed":"open");
+			//添加到列表中
+			resultList.add(node);
+		}
+		return resultList;
+	}
+
+}
